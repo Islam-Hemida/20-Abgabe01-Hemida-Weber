f19efb483ebaea2ff68fb57305f6414c7231ead9
Islam Hemida <isigamer@hotmail.com>
2020-05-13 22:14:24 +0200

    first test file

---
+++ src/test/java/at/fhj/iit/CocktailTest.java
@@ -0,0 +1,42 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CocktailTest {
+
+    @BeforeEach void setup(){
+
+    }
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void getAlcoholPercent() {
+    }
+
+    @Test
+    void isAlcoholic() {
+    }
+
+    @Test
+    void setVolume() {
+    }
+
+    @Test
+    void getMaxVolume() {
+    }
+
+    @Test
+    void setMaxVolume() {
+    }
+
+    @Test
+    void toMuchAlkohol() {
+       // assertEquals();
+    }
+}
