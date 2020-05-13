c6e50e7a95a82a2f2793550e9067c58d7fed4123
Islam Hemida <isigamer@hotmail.com>
2020-05-13 17:41:42 +0200

    add Cocktail.java

---
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -0,0 +1,16 @@
+package at.fhj.iit;
+
+import java.util.ArrayList;
+
+/**
+ * Class for  mixing a cocktail.
+ *
+ *
+ * @author      Lukas Weber
+ * @author      Islam Hemida
+ * @version     %I%, %G%
+ * @since       1.1
+ */
+
+public class Cocktail {
+}
