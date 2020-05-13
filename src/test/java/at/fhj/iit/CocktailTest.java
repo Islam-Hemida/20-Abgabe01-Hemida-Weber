1daece0379f12668c4269cb0456997eacd5d809e
iamWebLuk <lukas.weber@edu.fh-joanneum.at>
2020-05-13 22:17:29 +0200

    stashen

--- src/test/java/at/fhj/iit/CocktailTest.java
+++ src/test/java/at/fhj/iit/CocktailTest.java
@@ -1,5 +1,36 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CocktailTest {
 
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void getAlcoholPercent() {
+    }
+
+    @Test
+    void isAlcoholic() {
+    }
+
+    @Test
+    void setVolume() {
+    }
+
+    @Test
+    void getMaxVolume() {
+    }
+
+    @Test
+    void setMaxVolume() {
+    }
+
+    @Test
+    void toMuchAlkohol() {
+    }
 }
