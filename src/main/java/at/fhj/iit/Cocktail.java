02772782714281ea75846d262a03f2482414c9e1
iamWebLuk <lukas.weber@edu.fh-joanneum.at>
2020-05-13 18:08:57 +0200

    added some methods

--- src/main/java/at/fhj/iit/Cocktail.java
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -5,13 +5,37 @@ import java.util.ArrayList;
 /**
  * Class for  mixing a cocktail.
  *
- *
- * @author      Lukas Weber
- * @author      Islam Hemida
- * @version     %I%, %G%
- * @since       1.1
+ * @author Lukas Weber
+ * @author Islam Hemida
+ * @version %I%, %G%
+ * @since 1.1
  */
+// this is a subclass of drinks
+public class Cocktail extends Drink {
 
-public class Cocktail {
-    int x = 0;
+int volume;
+    /**
+     * Creates a Drink object with given name
+     *
+     * @param name name of the drink
+     */
+    public Cocktail(String name) {
+        super(name);
+    }
+
+    // you can have 0.2L and 0.5L
+    @Override
+    public double getVolume() {
+        return volume;
+    }
+
+    @Override
+    public double getAlcoholPercent() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAlcoholic() {
+        return false;
+    }
 }
